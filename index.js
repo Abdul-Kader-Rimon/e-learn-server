93bc5e13a1bce89e695bdaeb614269d18ec0ce28
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-12-07 23:07:07 +0600

    e-learn server create done

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
  
     const db = client.db("e-learn")
     const courseCollection = db.collection("courses")
@@ -119,7 +119,7 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
